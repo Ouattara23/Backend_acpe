aa3be87b2003804f76c7264ddf006e96ac9fb189
Ouattara23 <yahouattara64@gmail.com>
2025-03-28 08:40:43 +0000

    ajout de projet

--- models/post.model.js
+++ models/post.model.js
@@ -1,60 +1,60 @@
-const mongoose = require('mongoose');
-const postSchema = new  mongoose.Schema({
-     nom_ecole: {
-         type: String,
-         required: true     
-    } ,
-    logo_ecole:{
-         type: String,
-         required: true      
-    },
-    adresse_ecole:{
-         type: Number,
-         required: true      
-    },
-    ville_ecole:{
-         type: String,
-         required: true     
-    },
-    code_postal_ecole:{
-         type: String,
-         required: true     
-    },
-    nom_enseignant:{
-        type: String,
-        required: true
-    },  
+//const mongoose = require('mongoose');
+//const postSchema = new  mongoose.Schema({
+  //   nom_ecole: {
+    //     type: String,
+      //   required: true     
+    //} ,
+    //logo_ecole:{
+      //   type: String,
+        // required: true      
+    //},
+    //adresse_ecole:{
+      //   type: Number,
+        // required: true      
+    //},
+    //ville_ecole:{
+      //   type: String,
+        // required: true     
+    //},
+    //code_postal_ecole:{
+      //   type: String,
+        // required: true     
+    //},
+    //nom_enseignant:{
+      //  type: String,
+        //required: true
+    //},  
     
-    matiere_enseigne:{
-     type: String,
-     required: true
-    },
+    //matiere_enseigne:{
+     //type: String,
+     //required: true
+    //},
     
-    niveaux_scolaires:{
-        type: String,
-        required: true
-  },        
+    //niveaux_scolaires:{
+      //  type: String,
+        //required: true
+  //},        
 
 
-  email_admin:{
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, "Veuillez entrer une adresse email valide"]
-},        
+  //email_admin:{
+    //type: String,
+    //required: true,
+    //unique: true,
+    //match: [/.+@.+\..+/, "Veuillez entrer une adresse email valide"]
+//},        
 
 
-mot_de_passe:{
-    type: Number,
-    required: true
-},        
+//mot_de_passe:{
+  //  type: Number,
+    //required: true
+//},        
 
 
-confirmer_mot_de_passe:{
-    type: Number,
-    required: true
-}
+//confirmer_mot_de_passe:{
+  //  type: Number,
+    //required: true
+//}
 
-});
+//});
 
-module.exports = mongoose.model('post', postSchema);
+//module.exports = mongoose.model('post', postSchema);
